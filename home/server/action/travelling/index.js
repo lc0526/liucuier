2470b3dae9409686721991b889e1fb42d67fb17e
lc0526 <liu.cui.89@gmail.com>
2016-11-17 15:46:23 +0800

    home

---
+++ home/server/action/travelling/index.js
@@ -0,0 +1,8 @@
+module.exports = function(req, res, next) {
+	var query = req.query || {}; // (请求里面的键值对)
+	var data = {
+		query: query
+	};
+
+	res.render('home/page/travelling/index.tpl', data);
+};
